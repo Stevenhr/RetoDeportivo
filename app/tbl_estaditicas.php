59681dc1244bdd03bd1de241f973ee88b3de0556
David Marquez <carlinacastellanos4@gmail.com>
2018-05-12 12:06:50 -0500

    Modelos estadisticas, puntos persona

--- app/tbl_estaditicas.php
+++ app/tbl_estaditicas.php
@@ -4,8 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class tbl_estaditicas extends Model
-{
-     protected $table = 'tbl_estaditicas';
-    public $timestamps = false;
+class tbl_estaditicas extends Model{
+ 	protected $table = 'tbl_estaditicas';
+ 	protected $primayKey= 'i_pk_id_partido';
+ 	protected $fillable=['i_pk_id_partido', 'i_remates_equipo1', 'i_remates_equipo2', 'i_posesion_equipo1', 'i_posesion_equipo2', 'i_faltas_equipo1', 'i_faltas_equipo2', 'i_tarjeta_a_equipo1', 'i_tarjeta_a_equipo2', 'i_tarjeta_r_equipo1', 'i_tarjeta_r_equipo2', 'i_tiros_esquina_equipo1', 'i_tiros_esquina_equipo2'];
+ 	public function estadistica(){
+ 		return $this->belongsTo('App\tbl_partidos_por_evento','i_id_pk');
+ 	}
 }
--- app/tbl_puntos_persona.php
+++ app/tbl_puntos_persona.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class tbl_puntos_persona extends Model
 {
    protected $table = 'tbl_puntos_persona';
-    public $timestamps = false;
+   protected $primaryKey='i_pk_id';
+   protected $fillable= ['i_pk_id', 'i_puntaje'];
+   public function puntoPersona(){
+		return $this->belongsTo('App\tbl_pronosticos_personas','i_pk_id');
+   }
 }
